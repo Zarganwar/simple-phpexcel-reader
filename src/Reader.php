6486b25f5a3100a83d3e48b5ccaf2f3e66663665
Martin Jirasek <martin.jirasek@nms.cz>
2016-04-19 14:08:08 +0200

    init commit

---
+++ src/Reader.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SimplePHPExcelReader;
+/**
+ * Created by IntelliJ IDEA.
+ * User: Martin Jirásek <martin.jirasek@nms.cz>
+ * Date: 19.04.2016
+ * Time: 11:06
+ */
+class Reader
+{
+    /**
+     * @var string
+     */
+    private $filename;
+
+    /**
+     * @var PHPExcel
+     */
+    private $phpExcel;
+
+    /**
+     * @var array
+     */
+    private $data = [];
+
+    /**
+     * PHPExcelReader constructor.
+     * @param string $filename
+     */
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * @throws PHPExcel_Reader_Exception
+     */
+    private function loadPhpExcel()
+    {
+        $this->phpExcel = PHPExcel_IOFactory::createReader(PHPExcel_IOFactory::identify($this->filename))
+            ->load($this->filename);
+    }
+
+    /**
+     * @param int $fromRow
+     * @param bool|false $refresh
+     * @return array
+     */
+    public function read($fromRow = 1, $refresh = false)
+    {
+        if (!$this->phpExcel || $refresh) {
+            $this->loadPhpExcel();
+            $this->loadData($fromRow);
+        }
+        return $this->data;
+    }
+
+    /**
+     * @param $fromRow
+     * @throws PHPExcel_Exception
+     */
+    private function loadData($fromRow)
+    {
+        $sheet = $this->phpExcel->getSheet(0);
+        $highestRow = $sheet->getHighestRow();
+        $highestColumn = $sheet->getHighestColumn();
+        for ($row = $fromRow; $row <= $highestRow; $row++) {
+            $data = $sheet->rangeToArray("A$row:$highestColumn" . $row, NULL, TRUE, FALSE);
+            $this->data[] = $data[0];
+        }
+    }
+}
